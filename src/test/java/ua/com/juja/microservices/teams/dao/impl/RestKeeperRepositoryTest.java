6429cda3a6094bc528181f6c22846029604a0a6a
Moses-Odessa <moses2804@gmail.com>
2017-11-22 13:09:58 +0200

    42 restKeeperRepositoryTest made working

--- src/test/java/ua/com/juja/microservices/teams/dao/impl/RestKeeperRepositoryTest.java
+++ src/test/java/ua/com/juja/microservices/teams/dao/impl/RestKeeperRepositoryTest.java
@@ -1,5 +1,6 @@
 package ua.com.juja.microservices.teams.dao.impl;
 
+import feign.FeignException;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -7,12 +8,14 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 import ua.com.juja.microservices.teams.dao.KeeperRepository;
+import ua.com.juja.microservices.teams.dao.feign.KeeperClient;
 import ua.com.juja.microservices.teams.exceptions.KeeperExchangeException;
 
 import javax.inject.Inject;
@@ -22,6 +25,8 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withBadRequest;
@@ -38,9 +43,8 @@ public class RestKeeperRepositoryTest {
     public ExpectedException expectedException = ExpectedException.none();
     @Inject
     private KeeperRepository keeperRepository;
-    @Inject
-    private RestTemplate restTemplate;
-    private MockRestServiceServer mockServer;
+    @MockBean
+    private KeeperClient keeperClient;
 
     @Value("${keepers.endpoint.getDirections}")
     private String keepersGetDirectionsUrl;
@@ -48,32 +52,22 @@ public class RestKeeperRepositoryTest {
     @Value("${keepers.direction.teams}")
     private String teamsDirection;
 
-    @Before
-    public void setup() {
-        mockServer = MockRestServiceServer.bindTo(restTemplate).build();
-    }
 
     @Test
     public void getDirectionsExecutedCorrectly() {
         String uuid = "uuid";
-        mockServer.expect(requestTo(keepersGetDirectionsUrl + "/" + uuid))
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(withSuccess(
-                        "[\"First direction\",\"Second direction\"]",
-                        MediaType.APPLICATION_JSON));
         List<String> expected = Arrays.asList("First direction", "Second direction");
-
+        when(keeperClient.getDirections(uuid)).thenReturn(expected);
         List<String> actual = keeperRepository.getDirections(uuid);
-
         assertThat(actual, equalTo(expected));
     }
 
     @Test
     public void getDirectionsKeepersServiceReturnErrorThrowsException() {
         String uuid = "uuid";
-        mockServer.expect(requestTo(keepersGetDirectionsUrl + "/" + uuid))
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(withBadRequest().body("bad request"));
+        FeignException feignException = mock(FeignException.class);
+        when(keeperClient.getDirections(uuid)).thenThrow(feignException);
+        when(feignException.getMessage()).thenReturn("Keepers service returns error");
 
         expectedException.expect(KeeperExchangeException.class);
         expectedException.expectMessage(containsString("Keepers service returns error"));
